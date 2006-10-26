3a887662a6669ed42221b164473780b5b580c914
Victor Stinner <victor.stinner@haypocalc.com>
2006-10-26 09:47:30 +0000

    Create IPy repository with tags 0.4, 0.41 and 0.42

---
+++ setup.py
@@ -0,0 +1,10 @@
+# $Id: setup.py 671 2004-08-22 21:02:29Z md $
+from distutils.core import setup
+setup(name="IPy",
+      version="0.41",
+      description="IPv4 and IPv6 parsing and handling class",
+      author="drt",
+      author_email="drt@un.bewaff.net",
+      url="http://c0re.jp/c0de/Ipy/",
+      py_modules=["IPy"])
+
