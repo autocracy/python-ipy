531fe077b8d3a4aff5f418fae8941cbc1212afea
Victor Stinner <victor.stinner@haypocalc.com>
2006-10-26 16:15:30 +0000

    Tag version 0.5

--- Makefile
+++ Makefile
@@ -7,6 +7,9 @@ test: README
 	@echo "[ test README ]"
 	python -c "import doctest; doctest.testfile('README', optionflags=doctest.ELLIPSIS)" || exit $$?
 
+egg: clean README
+	python setup.py sdist bdist_egg
+
 README: IPy.py
 	python -c "import IPy; open('README', 'w').write(IPy.__doc__)"
 
@@ -14,5 +17,5 @@ IPy.html: README
 	rst2html README $@ --stylesheet=rest.css
 
 clean:
-	rm -f README IPy.html *.pyc
+	rm -rf README IPy.html *.pyc build dist IPy.egg-info
 
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python
 # $Id: setup.py 671 2004-08-22 21:02:29Z md $
+
 try:
     from setuptools.core import setup
 except ImportError:
     from distutils.core import setup
+
+import IPy
 
 LONG_DESCRIPTION=open('README').read()
 CLASSIFIERS = [
@@ -18,14 +22,17 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Natural Language :: English',
     'Programming Language :: Python']
-URL = "http://c0re.23.nu/c0de/IPy/"
+URL = "http://software.inl.fr/trac/trac.cgi/wiki/IPy"
 
 setup(name="IPy",
       version=IPy.__version__,
       description="IPv4 and IPv6 parsing and handling class",
       long_description=LONG_DESCRIPTION,
       author="drt",
-      author_email="drt@un.bewaff.net",
+      maintainer="Victor Stinner",
+      maintainer_email="victor.stinner AT inl.fr",
+      license="BSD License",
+      keywords="ipv4 ipv6 netmask",
       url=URL,
       download_url=URL,
       classifiers= CLASSIFIERS,
