73775a0d2ca6f84371528fba795c87836e5f445e
Victor Stinner <victor.stinner@haypocalc.com>
2007-04-02 08:49:22 +0000

    Apply second patch of James Teh: add make_net() method

--- IPy.py
+++ IPy.py
@@ -841,6 +841,19 @@ class IP(IPint):
         else:
             raise ValueError, "only IPv4 and IPv6 supported"
 
+    def make_net(self, netmask):
+        """Transform a single IP address into a network specification by
+        applying the given netmask.
+
+        Returns a new IP instance.
+
+        >>> print IP('127.0.0.1').make_net('255.0.0.0')
+        127.0.0.0/8
+        """
+        if '/' in str(netmask):
+            raise ValueError, "invalid netmask (%s)" % netmask
+        return IP('%s/%s' % (self, netmask), make_net=True)
+
     def __getitem__(self, key):
         """Called to implement evaluation of self[key].
 
